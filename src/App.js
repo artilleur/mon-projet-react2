2faa6683e9b1f50edf5586161881bbfdd6fd8e73
artilleur <99259153+artilleur@users.noreply.github.com>
2023-11-21 09:42:02 +0100

    123456789

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import TodoList from "./TodoList";
+import { useState } from "react";
+
 
 function App() {
+
+  const [data, setData] = useState(["boire", "rigoler", "manger", "dormir"])
+
+  const handleAdd = (text) => {
+    setData([...data, text])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="composant">
+      <h1>App</h1>
+
+      <Header  list={data}/>
+      <TodoList list={data} onAdd={handleAdd}/>
     </div>
   );
 }
